c9d9f093fd474fda6dd16f75fceeb7943fdc7544
Kareem Mohamed <karem.mohamed0010@gmail.com>
2019-04-22 18:49:58 +0200

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,23 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { GradientComponent } from './components/gradient/gradient.component';
+import { BoxshadowComponent } from './components/boxshadow/boxshadow.component';
+import { AboutComponent } from './components/about/about.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'gradient',
+    component: GradientComponent
+  },
+  {
+    path: 'box-shadow',
+    component:BoxshadowComponent
+  },
+ {
+  path: 'about',
+  component:AboutComponent
+},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { ColorPickerModule } from 'ngx-color-picker';
+
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { GradientComponent } from './components/gradient/gradient.component';
+import { BoxshadowComponent } from './components/boxshadow/boxshadow.component';
+import { AboutComponent } from './components/about/about.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavbarComponent,
+    GradientComponent,
+    BoxshadowComponent,
+    AboutComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ColorPickerModule
   ],
   providers: [],
   bootstrap: [AppComponent]
